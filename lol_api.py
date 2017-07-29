bcaf4781d0bfa81e12ae628e3464c69d3af61820
Vlad Barinov <step_aside@bk.ru>
2017-07-29 22:02:38 +0300

    start

--- lol_api.py
+++ lol_api.py
@@ -4,6 +4,7 @@ import config
 watcher = RiotWatcher(config.riot_token)
 
 my_region = 'ru'
+sumname = 'Barichpock'
 
 
 def summoner(sumname):
@@ -12,8 +13,21 @@ def summoner(sumname):
     return str(me['id'])
 
 
-def ranked(sumname):
-    my_ranked_stats = watcher.league.positions_by_summoner(my_region, summoner(sumname))
-    return(my_ranked_stats[0])
+# def ranked(sumname):
+#     my_ranked_stats = watcher.league.positions_by_summoner(my_region, summoner(sumname))
+#     return(my_ranked_stats[0])
 
-# ranked()
+
+def ranked(sumname):
+    my_ranked_stats = watcher.league.positions_by_summoner(my_region, int(summoner(sumname)))
+    text = "Твой дивизион: {} \n" \
+           "Твой ранг: {} \n" \
+           "Твои очки лиги: {} \n".format(my_ranked_stats[0]['tier'],
+                                          my_ranked_stats[0]['rank'],
+                                          str(my_ranked_stats[0]['leaguePoints']))
+    print(text)
+    print(my_ranked_stats)
+    return text
+
+
+ranked(sumname)
