b588749450a5a30c42cc2948f9dabff377073da4
Олег Сидоренков <oleg.sidorenkov.92@mail.ru>
2017-07-30 13:05:12 +0300

    2 commands: start, summoner

--- bot.py
+++ bot.py
@@ -2,29 +2,48 @@
 
 import telebot
 import config
+import sqlite3
+import os
 import time
 from telebot import types
 import lol_api
 
-knownUsers = {}  # todo: save these in a file,
-userStep = {}  # so they won't reset every time the bot restarts
-
 commands = {  # command description used in the "help" command
               'start': 'Get used to the bot',
-              'help': 'Gives you information about the available commands',
-              'sendLongText': 'A test using the \'send_chat_action\' command',
-              'getImage': 'A test using multi-stage messages, custom keyboard, and media sending'
+              'help': 'Даст информацию о доступных командах',
+              'getImage': 'A test using multi-stage messages, custom keyboard, and media sending',
+              'summoner': 'Поменять имя призывателя'
 }
 
+hideBoard = types.ReplyKeyboardRemove()
 
-def get_user_step(uid):
-    if uid in userStep:
-        return userStep[uid]
+
+def get_user(cid):
+    conn = sqlite3.connect(os.path.join(os.path.dirname(__file__), 'bot.db'))
+    c = conn.cursor()
+    c.execute('''CREATE TABLE if not exists users
+                (chat_id integer, summoner text)''')
+    conn.commit()
+    c.execute('SELECT summoner FROM users WHERE chat_id=?', (cid,))
+    result = c.fetchall()
+    if not result:
+        return result
     else:
-        knownUsers.append(uid)
-        userStep[uid] = 0
-        print("New user detected, who hasn't used \"/start\" yet")
-        return 0
+        return result[0][0]
+
+
+def append_user(cid, summoner):
+    conn = sqlite3.connect(os.path.join(os.path.dirname(__file__), 'bot.db'))
+    c = conn.cursor()
+    c.execute("INSERT INTO users VALUES (?, ?);", (cid, summoner))
+    conn.commit()
+
+
+def update_user(cid, summoner):
+    conn = sqlite3.connect(os.path.join(os.path.dirname(__file__), 'bot.db'))
+    c = conn.cursor()
+    c.execute("UPDATE users SET summoner=? WHERE chat_id=?;", (summoner, cid))
+    conn.commit()
 
 
 # only used for console output now
@@ -39,63 +58,65 @@ def listener(messages):
 
 
 bot = telebot.TeleBot(config.bot_token)
+bot.set_update_listener(listener)  # register listener
 
 
 @bot.message_handler(commands=['start'])
 def command_start(m):
     cid = m.chat.id
-    bot.send_message(cid, "Рад тебя видеть чампион! Светани свой ник, чтобы я заценил как ты тащишь катки =)")
+    user = get_user(cid)
+    if not user:
+        bot.send_message(cid, "Рад тебя видеть чампион! Напиши свой ник, чтобы я заценил как ты тащишь катки =)")
+        bot.register_next_step_handler(m, step_append_user)
+    else:
+        bot.send_message(cid, "Рад тебя снова видеть чампион! Я тебя знаю, ты {}".format(user))
+        bot.send_chat_action(cid, 'typing')  # show the bot "typing" (max. 5 secs)
+        time.sleep(1)
+        bot.send_message(cid, "Введи /help для просмотра доступных команд.")
+
+
+def step_append_user(m):
+    cid = m.chat.id
+    name = m.text
+    append_user(cid, name)
+    bot.send_message(cid, "Я добавил тебя в свой блокнотик.")
+    bot.send_chat_action(cid, 'typing')  # show the bot "typing" (max. 5 secs)
+    time.sleep(1)
+    bot.send_message(cid, "Введи /help для просмотра доступных команд.")
 
 
 # help page
 @bot.message_handler(commands=['help'])
 def command_help(m):
     cid = m.chat.id
-    help_text = "The following commands are available: \n"
+    help_text = "Доступны следующие команды: \n"
     for key in commands:  # generate help text out of the commands dictionary defined at the top
         help_text += "/" + key + ": "
         help_text += commands[key] + "\n"
     bot.send_message(cid, help_text)  # send the generated help page
 
 
-# chat_action example (not a good one...)
-@bot.message_handler(commands=['sendLongText'])
-def command_long_text(m):
+@bot.message_handler(commands=['summoner'])
+def command_summoner(m):
     cid = m.chat.id
-    bot.send_message(cid, "If you think so...")
-    bot.send_chat_action(cid, 'typing')  # show the bot "typing" (max. 5 secs)
-    time.sleep(3)
-    bot.send_message(cid, ".")
-
-
-@bot.message_handler(func=lambda message: message.text == "Привет")
-def command_text_hi(m):
-    bot.send_message(m.chat.id, "Ку-ку епта!")
-
-
-# default handler for every other text
-@bot.message_handler(func=lambda message: True, content_types=['text'])
-def command_default(m):
-    cid = m.chat.id
-    sumname = m.text
     # this is the standard reply to a normal message
-    lol_api.summoner(sumname)
-    knownUsers[cid] = sumname
     bot.send_chat_action(cid, 'typing')  # show the bot "typing" (max. 5 secs)
     time.sleep(1)
-    bot.send_message(m.chat.id, "Чекнул.. Конечно, я ожидал большего")
-    bot.send_chat_action(cid, 'typing')  # show the bot "typing" (max. 5 secs)
-    time.sleep(1)
-    bot.send_message(m.chat.id, "Твой левел = " + lol_api.summoner(sumname))
+    msg = bot.reply_to(m, "Введи имя призывателя")
+    bot.register_next_step_handler(msg, step_summoner)
 
-    # bot.send_message(m.chat.id, "I don't understand \"" + m.text + "\"\nMaybe try the help page at /help")
+
+def step_summoner(m):
+    cid = m.chat.id
+    name = m.text
+    update_user(cid, name)
+    bot.send_message(cid, "Имя призывателя изменено!")
 
 
 @bot.message_handler(commands=['ranked'])
 def command_ranked(m):
     cid = m.chat.id
-    sumname = knownUsers[cid]
-    print(sumname)
+    sumname = get_user(cid)
     # this is the standard reply to a normal message
     bot.send_chat_action(cid, 'typing')  # show the bot "typing" (max. 5 secs)
     time.sleep(1)
@@ -112,11 +133,10 @@ def command_ranked(m):
 #     bot.send_message(message.chat.id, "Выбери, что ты хочешь:", reply_markup=markup)
 
 
-# @bot.message_handler(commands=['summoner'])
-# def command_summoner(message):
-#     summoner = bot.send_message(message.chat.id, "Напиши имя своего чампа:")
-#     bot.register_next_step_handler(summoner, local)
+# default handler for every other text
+# @bot.message_handler(func=lambda message: True, content_types=['text'])
+# def command_default(m):
+#     bot.send_message(m.chat.id, "I don't understand \"" + m.text + "\"\nMaybe try the help page at /help")
 
 
-if __name__ == '__main__':
-    bot.polling(none_stop=True)
+bot.polling(none_stop=True)
